be376325c3ecedfa6d1dfac98e157ac1cb647019
Aman Shaikh <amanshaikh205276@gmail.com>
2023-11-25 20:43:04 -0500

    Checkbox selection done

--- src/main/java/RentalAnalysis/Crawler.java
+++ src/main/java/RentalAnalysis/Crawler.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -16,8 +17,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.Duration;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Crawler {
 
@@ -32,9 +32,6 @@ public class Crawler {
 
 		ChromeOptions options = new ChromeOptions();
 		 options.addArguments("--start-maximized");
-
-
-
 
 		try {
 
@@ -69,23 +66,27 @@ public class Crawler {
 				}
 				searchBox.sendKeys(city);
 				// Wait for the auto-suggestion dropdown to appear
-				Thread.sleep(500);
+				Thread.sleep(1500);
 				searchBox.sendKeys(Keys.ENTER);
 
 				// Wait for some time to let the search results load
 				Thread.sleep(2000);
 
 				// Select the type of house
-//				selectCheckboxInDropdown(driver, "typeDropdown", type); // Replace with the actual name or ID of the type dropdown
+				selectType(driver, "//*[@id=\"home_search_top\"]/ul/li[4]", type); // Replace with the actual name or ID of the beds dropdown
 
 
+				// Wait for some time to let the search results load
+				Thread.sleep(2000);
 
-				// Select the number of beds
+				// Select the type of house
+
+				// Select the number of beds//*[@id="home_search_top"]/ul/li[4]/div/ul/li[1]/label/span
 				selectBeds(driver, "//*[@id=\"home_search_top\"]/ul/li[3]", beds); // Replace with the actual name or ID of the beds dropdown
 
 
 				// Crawl all listings and store data in text files
-				// crawlListingsAndStoreData(driver);
+				 crawlListingsAndStoreData(driver,city);
 
 
 				System.out.print("\n\nDo you want to continue y/n: ");
@@ -118,8 +119,44 @@ public class Crawler {
 		WebElement bedOption = driver.findElement(By.xpath(dropDownXPath+"/div/div[2]/ul//a[contains(text(),\""+ value +"+\")"+"]"));
 		bedOption.click();
 	}
+	private static void selectType(WebDriver driver, String dropDownXPath, String value) {
+		WebElement dropdown = driver.findElement(By.xpath(dropDownXPath+"/a")); // Replace with the actual ID of the dropdown
+		dropdown.click();
 
-	private static void crawlListingsAndStoreData(WebDriver driver) {
+		// Wait for some time to let the dropdown options load
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+
+		// Split the comma-separated values
+		String[] typesToSelect = value.split(",");
+		for (String type : typesToSelect) {
+
+			if(type.equalsIgnoreCase("house")){
+				driver.findElement(By.xpath("//*[@id=\"home_search_top\"]/ul/li[4]/div/ul/li[1]")).click();
+			}
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			if(type.equalsIgnoreCase("condo")){
+				driver.findElement(By.xpath("//*[@id=\"home_search_top\"]/ul/li[4]/div/ul/li[2]")).click();
+			}
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			if(type.equalsIgnoreCase("townhouse")){
+				driver.findElement(By.xpath("//*[@id=\"home_search_top\"]/ul/li[4]/div/ul/li[3]")).click();
+			}
+		}
+	}
+	private static void crawlListingsAndStoreData(WebDriver driver, String city) {
 		// Assume each listing is represented by a WebElement with a class "listing-item"
 		List<WebElement> listings = driver.findElements(By.className("listing-item"));
 
@@ -129,41 +166,43 @@ public class Crawler {
 			return;
 		}
 
+		// Create a StringBuilder to store all the data as a string
+		StringBuilder allDataStringBuilder = new StringBuilder();
+
+		// Loop through each listing
+		for (int i = 0; i < listings.size(); i++) {
+			WebElement listing = listings.get(i);
+
+			// Extract data from the listing as strings
+			String numberOfBeds = listing.findElement(By.xpath(".//ul/li[2]")).getText();
+			String numberOfBaths = listing.findElement(By.xpath(".//ul/li[3]")).getText();
+			String squareFeet = listing.findElement(By.xpath(".//ul/li[4]")).getText();
+			String price = listing.findElement(By.xpath(".//ul/li[1]/span[2]")).getText();
+
+			// Append data to the StringBuilder
+			allDataStringBuilder.append("City: ").append(city).append("\n");
+			allDataStringBuilder.append("Number of Beds: ").append(numberOfBeds).append("\n");
+			allDataStringBuilder.append("Number of Baths: ").append(numberOfBaths).append("\n");
+			allDataStringBuilder.append("Square Feet: ").append(squareFeet).append("\n");
+			allDataStringBuilder.append("Price: ").append(price).append("\n");
+			allDataStringBuilder.append("\n");
+		}
+
 		// Create a directory to store text files
 		File directory = new File("assets/txtfiles");
 		if (!directory.exists()) {
 			directory.mkdirs();
 		}
 
-		for (int i = 0; i < listings.size(); i++) {
-			WebElement listing = listings.get(i);
+		// Create a text file for all listings
+		File txtFile = new File("assets/txtfiles/all_listings.txt");
 
-			// Extract data from the listing
-			String listingTitle = safeGetText(listing.findElement(By.className("title")));
-			String listingDescription = safeGetText(listing.findElement(By.className("description")));
-			// Add more fields as needed...
-
-			// Create a text file for each listing
-			File txtFile = new File("assets/txtfiles/listing_" + i + ".txt");
-
-			try (BufferedWriter writer = new BufferedWriter(new FileWriter(txtFile))) {
-				// Write data to the text file
-				writer.write("Listing Title: " + listingTitle);
-				writer.newLine();
-				writer.write("Listing Description: " + listingDescription);
-				// Add more lines for additional fields...
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(txtFile))) {
+			// Write all data to the text file
+			writer.write(allDataStringBuilder.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
-	private static String safeGetText(WebElement element) {
-		try {
-			return element.getText();
-		} catch (Exception e) {
-			System.out.println("Error getting text from element: " + e.getMessage());
-			return "";
-		}
-	}
 }
